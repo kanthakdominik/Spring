10866c318f9ef37605aa9ecf330fd84d9a0dce58
kanthakdominik <kanthakdominik@gmail.com>
2020-05-16 17:38:18 +0200

    java code without spring

---
+++ conference/src/main/java/Application.java
@@ -0,0 +1,11 @@
+import pl.dominik.service.SpeakerService;
+import pl.dominik.service.SpeakerServiceImpl;
+
+public class Application {
+
+    public static void main(String args[]) {
+        SpeakerService service = new SpeakerServiceImpl();
+
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
+}
