94dab359a747260aa93f3f87f25a7149594f7cb4
kanthakdominik <kanthakdominik@gmail.com>
2020-05-20 21:15:24 +0200

    add REST controller example add jQuery view example

---
+++ conference/src/main/java/pl/dominik/conference/model/User.java
@@ -0,0 +1,32 @@
+package pl.dominik.conference.model;
+
+public class User {
+
+    private String firsName;
+    private String lastName;
+    private int age;
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
