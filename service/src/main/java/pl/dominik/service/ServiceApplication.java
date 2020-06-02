7261de2cb5e6b979ea7a38c611290b2deb856204
kanthakdominik <kanthakdominik@gmail.com>
2020-06-02 21:32:45 +0200

    add services

---
+++ service/src/main/java/pl/dominik/service/ServiceApplication.java
@@ -0,0 +1,25 @@
+package pl.dominik.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class ServiceApplication {
+
+    @Value("${service.instance.name}")
+    private String instance;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+    public String message(){
+        return "Hello from " + instance;
+    }
+
+}
