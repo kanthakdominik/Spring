eef4b47b2d0fe74f1ca8fd2f870dcac9169fa84f
kanthakdominik <kanthakdominik@gmail.com>
2020-06-03 01:09:07 +0200

    add RequestMapping annotation to service

--- service/src/main/java/pl/dominik/service/ServiceApplication.java
+++ service/src/main/java/pl/dominik/service/ServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
@@ -18,6 +19,7 @@ public class ServiceApplication {
         SpringApplication.run(ServiceApplication.class, args);
     }
 
+    @RequestMapping("/")
     public String message(){
         return "Hello from " + instance;
     }
