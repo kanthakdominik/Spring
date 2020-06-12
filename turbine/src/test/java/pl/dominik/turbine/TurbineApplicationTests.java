d5a70aa9628ab1725cb3c92bd2d6c2d79a1192dc
kanthakdominik <kanthakdominik@gmail.com>
2020-06-12 14:32:58 +0200

    add aggregation of multiple hystrix dashboard by turbine

---
+++ turbine/src/test/java/pl/dominik/turbine/TurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dominik.turbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
