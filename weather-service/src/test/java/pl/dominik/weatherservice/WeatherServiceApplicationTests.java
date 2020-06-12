7a83adfe17ce9c67350631986f26b4d3b348664d
kanthakdominik <kanthakdominik@gmail.com>
2020-06-12 13:33:14 +0200

    add fault tolerance with netflix hystrix

---
+++ weather-service/src/test/java/pl/dominik/weatherservice/WeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dominik.weatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
